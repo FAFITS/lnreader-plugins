3e05e2b15ad9b94f1dc9b627912456f39338646a
FAFITS <famukonkiri@gmail.com>
2026-04-14 18:06:51 +0700

    test test

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,5 +1,6 @@
 import { Plugin } from '@/types/plugin';
 import p_0 from '@plugins/english/novelbin';
+import p_1 from '@plugins/vietnamese/doctruyenln';
 
-const PLUGINS: Plugin.PluginBase[] = [p_0];
+const PLUGINS: Plugin.PluginBase[] = [p_0, p_1];
 export default PLUGINS;

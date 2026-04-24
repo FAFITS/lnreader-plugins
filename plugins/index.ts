25fe82fb2faf35a006c80648dc4d4037cc0983e7
FAFITS <famukonkiri@gmail.com>
2026-04-24 18:11:26 +0700

    done

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,6 +1,5 @@
 import { Plugin } from '@/types/plugin';
 import p_0 from '@plugins/english/novelbin';
-import p_1 from '@plugins/multi/jukanovel';
 
-const PLUGINS: Plugin.PluginBase[] = [p_0, p_1];
+const PLUGINS: Plugin.PluginBase[] = [p_0];
 export default PLUGINS;
